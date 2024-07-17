917c30188d172c373b317f90a6dd92fcf7daf795
Serge Romanchuk <theerror133@gmail.com>
2024-07-17 18:34:54 +0300

    lspconfig для javascript + новая тема

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -6,11 +6,17 @@ return {
     priority = 1000,
     opts = {},
   },
+  {
+    "loctvl842/monokai-pro.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
   -- Load Colorscheme.
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "solarized-osaka",
+      colorscheme = "monokai-pro",
     },
   },
 }
