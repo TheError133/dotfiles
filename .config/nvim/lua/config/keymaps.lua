c5f4b9ef1d5aeea523f2a7794d8d77df242948a5
Serge Romanchuk <theerror133@gmail.com>
2024-07-17 18:22:26 +0300

    Новые keymaps.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,13 +2,16 @@ local keymap = vim.keymap
 local opts = { silent = true, noremap = true }
 
 -- Delete word backwards.
-keymap.set("n", "dw", "vbd")
+-- keymap.set("n", "dw", "vbd")
+
+-- Select all.
+keymap.set("n", "<C-a>", "<S-g>Vgg")
 
 -- Tabs.
-keymap.set("n", "te", ":tabedit<Return>", opts)
-keymap.set("n", "tc", ":tabclose<Return>", opts)
-keymap.set("n", "<tab>", ":tabnext<Return>", opts)
-keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+-- keymap.set("n", "te", ":tabedit<Return>", opts)
+-- keymap.set("n", "tc", ":tabclose<Return>", opts)
+-- keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+-- keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
 
 -- Windows.
 keymap.set("n", "<C-h>", "<C-w>h")
@@ -21,3 +24,8 @@ keymap.set("n", "sh", ":split<Return>")
 keymap.set("n", "sv", ":vsplit<Return>")
 keymap.set("n", "sq", "<C-w>q")
 keymap.set("n", "sw", "<C-w>w")
+
+-- Buffers.
+keymap.set("n", "<tab>", ":bn<Return>", opts)
+keymap.set("n", "<s-tab>", ":bp<Return>", opts)
+keymap.set("n", "Q", ":bd<Return>", opts)
